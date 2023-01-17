acf89273203bf62330f55ba61235e0578ad616e5
Chad Scribner <chadbscribner@gmail.com>
2023-01-16 19:02:23 -0600

    DOOM capitalize

--- lib/special.example.sh
+++ lib/special.example.sh
@@ -4,15 +4,15 @@
 
 readonly -A special_items=(
     ["Aleste 2 - Compile (1989) [ROM Version] [v8 by Ricbit] [3275].rom"]="Microsoft - MSX;Aleste 2 - Compile (1989) [ROM Version] [v8 by Ricbit] [3275].rom;fmsx_libretro.so"
-    ["Doom - Sigil.sh"]="Doom;doom/sigil/DOOM.WAD;prboom_libretro.so"
-    ["Doom 2.sh"]="Doom;doom/doom2/DOOM2.WAD;prboom_libretro.so"
-    ["Final Doom - The Plutonia Experiment.sh"]="Doom;doom/PLUTONIA/PLUTONIA.WAD;prboom_libretro.so"
-    ["Final Doom - The Plutonia Experiment.sh"]="Doom;doom/final-doom-plutonia-experiment/PLUTONIA.WAD;prboom_libretro.so"
-    ["Final Doom - TNT - Evilution.sh"]="Doom;doom/final-doom-tnt-evilution/TNT.WAD;prboom_libretro.so"
+    ["Doom - Sigil.sh"]="DOOM;doom/sigil/DOOM.WAD;prboom_libretro.so"
+    ["Doom 2.sh"]="DOOM;doom/doom2/DOOM2.WAD;prboom_libretro.so"
+    ["Final Doom - The Plutonia Experiment.sh"]="DOOM;doom/PLUTONIA/PLUTONIA.WAD;prboom_libretro.so"
+    ["Final Doom - The Plutonia Experiment.sh"]="DOOM;doom/final-doom-plutonia-experiment/PLUTONIA.WAD;prboom_libretro.so"
+    ["Final Doom - TNT - Evilution.sh"]="DOOM;doom/final-doom-tnt-evilution/TNT.WAD;prboom_libretro.so"
     ["Quake Episode 5 (dopa).sh"]="Quake;quake/dopa/pak0.pak;tyrquake_libretro.so"
     ["Quake Mission Pack 1 (hipnotic).sh"]="Quake;quake/hypnotic/pak0.pak;tyrquake_libretro.so"
     ["Quake Mission Pack 2 (rogue).sh"]="Quake;quake/rogue/pak0.pak;tyrquake_libretro.so"
     ["Quake.sh"]="Quake;quake/id1/pak0.pak;tyrquake_libretro.so"
     ["Ninja Ryuuken Den III - Yomi no Hakobune (Japan).nes"]="Nintendo - Nintendo Entertainment System;Ninja Ryuuken Den III - Yomi no Hakobune (Japan).nes;fceumm_libretro.so"
-    ["Ultimate Doom.sh"]="Doom;doom/ultimate-doom/DOOM.WAD;prboom_libretro.so"
+    ["Ultimate Doom.sh"]="DOOM;doom/ultimate-doom/DOOM.WAD;prboom_libretro.so"
 )
