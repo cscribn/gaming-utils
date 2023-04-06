b0dee8c81fc5ce5cc57411939192d53290cc1b7b
Chad Scribner <chadbscribner@gmail.com>
2023-04-06 09:13:25 -0500

    Removing CHAMP games

--- lib/special.example.sh
+++ lib/special.example.sh
@@ -5,13 +5,6 @@
 readonly -A special_items=(
 	["Aleste 2 - Compile (1989) [ROM Version] [v8 by Ricbit] [3275].rom"]="Microsoft - MSX;Aleste 2 - Compile (1989) [ROM Version] [v8 by Ricbit] [3275].rom;km_fmsx_libretro.so"
 	["Battlezone [DC] (1983)(Atarisoft) [Action, Simulation].sh"]="DOS;Battlezone [DC] (1983)(Atarisoft) [Action, Simulation]/ZONE.EXE;km_dosbox_pure_libretro.so"
-	["CHAMP Centiped-em (1997)(CHAMProgramming).sh"]="DOS;CHAMP Centiped-em (1997)(CHAMProgramming)/CHAMP/CENTIPED/CENTIPED.EXE;km_dosbox_pure_libretro.so"
-	["CHAMP Galagon (1997)(CHAMProgramming).sh"]="DOS;CHAMP Galagon (1997)(CHAMProgramming)/CHGalag/CHAMP/GALAGON/GALAGON.EXE;km_dosbox_pure_libretro.so"
-	["CHAMP Galaxia (1996)(CHAMProgramming).sh"]="DOS;CHAMP Galaxia (1996)(CHAMProgramming)/CHGalax/CHAMP/GALAXIA/GALAXIA.EXE;km_dosbox_pure_libretro.so"
-	["CHAMP Invaders (1997)(CHAMProgramming).sh"]="DOS;CHAMP Invaders (1997)(CHAMProgramming)/CHAMP/INVADERS/INVADERS.EXE;km_dosbox_pure_libretro.so"
-	["CHAMP Kong (1996)(CHAMProgramming).sh"]="DOS;CHAMP Kong (1996)(CHAMProgramming)/KONG.EXE;km_dosbox_pure_libretro.so"
-	["CHAMP Ms. Pac-em (1997)(CHAMProgramming).sh"]="DOS;CHAMP Ms. Pac-em (1997)(CHAMProgramming)/CHAMP/MSPACEM/MSPACEM.EXE;km_dosbox_pure_libretro.so"
-	["CHAMP Pac-em (1997)(CHAMProgramming).sh"]="DOS;CHAMP Pac-em (1997)(CHAMProgramming)/CHAMP/PACEM/PACEM.EXE;km_dosbox_pure_libretro.so"
 	["Doom - Sigil.sh"]="Doom;doom/sigil/DOOM.WAD;km_prboom_xtreme_libretro.so"
 	["Doom 2.sh"]="Doom;doom/doom2/DOOM2.WAD;km_prboom_xtreme_libretro.so"
 	["Final Doom - The Plutonia Experiment.sh"]="Doom;doom/final-doom-plutonia-experiment/PLUTONIA.WAD;km_prboom_xtreme_libretro.so"
