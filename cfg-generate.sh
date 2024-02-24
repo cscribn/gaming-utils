18d8a4c6414b2edb5fffc6f095a02e2ecf92ee20
Chad Scribner <chadbscribner@gmail.com>
2024-02-24 15:42:25 -0600

    Reverting back to content directory override

--- cfg-generate.sh
+++ cfg-generate.sh
@@ -17,6 +17,7 @@ declare dir_cfg
 declare machine
 declare machine_cfg_dir
 declare system
+declare system_no_under
 
 readonly -A input_turbo_default_values=(
 	["a"]="2"
@@ -93,14 +94,14 @@ core_options_gen() {
 	echo "${script_name}: ${machine} - core options generate - ${corename}"
 
 	if printf '%s\0' "${!corename_core_options[@]}" | grep -Fxqz "$corename"; then
-		grep "${corename_core_options[${corename}]}" "$core_opts_cfg_source" > "${machine_cfg_dir}/${corename}/${corename}.opt"
+		grep "${corename_core_options[${corename}]}" "$core_opts_cfg_source" > "${machine_cfg_dir}/${corename}/${system_no_under}.opt"
 	else
-		true > "${machine_cfg_dir}/${corename}/${corename}.opt"
+		true > "${machine_cfg_dir}/${corename}/${system_no_under}.opt"
 	fi
 
 	if [[ "$machine" != "retro"* ]] && printf '%s\0' "${!dupe_cfg_dirs[@]}" | grep -Fxqz "$corename"; then
-		cp "${machine_cfg_dir}/${corename}/${corename}.opt" \
-			"${machine_cfg_dir}/${corename}/${dupe_cfg_dirs[${corename}]}.opt"
+		cp "${machine_cfg_dir}/${corename}/${system_no_under}.opt" \
+			"${machine_cfg_dir}/${dupe_cfg_dirs[${corename}]}/${system_no_under}.opt"
 	fi
 }
 
@@ -210,6 +211,7 @@ main() {
 
 	for system in "${!system_retro_corenames[@]}"; do
 		corename="${system_retro_corenames[${system}]}"
+		system_no_under="${system%_*}"
 
 		cfg_init
 		[[ "$machine" = "retro"* ]] && core_cfg_gen
