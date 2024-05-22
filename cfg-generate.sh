0908ed57524303160005cf0052772c7cdaa14c39
Chad Scribner <chadbscribner@gmail.com>
2024-05-22 13:11:16 -0500

    Adding retroarch directory replacement

--- cfg-generate.sh
+++ cfg-generate.sh
@@ -15,6 +15,7 @@ declare cfg_dir
 declare core_opts_cfg_source
 declare machine
 declare machine_cfg_dir
+declare retroarch_dir
 declare system
 declare system_no_under
 
@@ -78,6 +79,7 @@ check_new_clean() {
 		exit 0
 	fi
 
+	# set machine_cfg_dirs
 	if [[ "$machine" = "retropad" ]]; then
 		machine_cfg_dir="${cfg_dir}/mirror/${machine}/home/ark/.config/retroarch/config"
 	elif [[ "$machine" = "retro"* ]]; then
@@ -88,6 +90,19 @@ check_new_clean() {
 		machine_cfg_dir="${cfg_dir}/mirror/${machine}/retroarch/config"
 	elif [[ "$machine" = "segamini" ]]; then
 		machine_cfg_dir="${cfg_dir}/mirror/${machine}/hakchi/libretro/config"
+	fi
+
+	# set retroarch_dirs
+	if [[ "$machine" = "retropad" ]]; then
+		retroarch_dir="~\/\.config\/retroarch"
+	elif [[ "$machine" = "retro"* ]]; then
+		retroarch_dir="\/opt\/retropie\/configs\/all\/retroarch"
+	elif [[ "$machine" = "a500" ]]; then
+		retroarch_dir="~\/\.config\/retroarch"
+	elif [[ "$machine" = "psclassic" ]]; then
+		retroarch_dir=":"
+	elif [[ "$machine" = "segamini" ]]; then
+		retroarch_dir="~"
 	fi
 
 	echo "${script_name}: ${machine} - cleaning machine cfgs"
@@ -138,6 +153,11 @@ machine_remaps_dir_get() {
 	fi
 }
 
+retroarch_dir_replace() {
+	echo "${script_name}: ${machine} - retroarch dir replace"
+	sed -i "s/\$RETROARCH_DIR/${retroarch_dir}/g" "${machine_cfg_dir}"/*/*.cfg
+}
+
 turbo_button_replace() {
 	echo "${script_name}: ${machine} - turbo button replace - a"
 	sed -i "s/_turbo_btn = \"a\"/_turbo_btn = \"${input_turbo_btn_values[${machine};a]}\"/g" "${machine_cfg_dir}"/*/*.cfg
@@ -163,6 +183,7 @@ main() {
 	check_new_clean
 	all_cfg_cp
 	turbo_button_replace
+	retroarch_dir_replace
 
 	for system in "${!system_retro_corenames[@]}"; do
 		corename=$(machine_cfg_dir_get "${system_retro_corenames[$system]}")
