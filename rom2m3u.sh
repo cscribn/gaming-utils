887a01e0e62d7c7b0b5dc754b787cef7344e623f
Chad Scribner <chadbscribner@gmail.com>
2024-08-18 10:25:39 -0500

    Moving locals to top. Rewriting globals to all caps.

--- rom2m3u.sh
+++ rom2m3u.sh
@@ -6,22 +6,22 @@ set -o pipefail
 [[ "${TRACE-0}" = "1" ]] && set -o xtrace
 
 # global variables
-declare script_name
-script_name="$(basename "${0}")"
-declare script_dir
-script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-declare rom_dir
+SCRIPT_NAME="$(basename "${0}")"
+readonly SCRIPT_NAME
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+readonly SCRIPT_DIR
 
 # usage
 if [[ "${1-}" =~ ^-*h(elp)?$ ]]; then
-    echo "Usage: ./${script_name} rom_path"
+    echo "Usage: ./${SCRIPT_NAME} rom_path"
     exit
 fi
 
 # main function
 main() {
+	local rom_dir="$1"
+
 	# check_inputs
-	rom_dir="$1"
     [[ "$rom_dir" = "" ]] && echo "Missing rom_dir" && exit 1
 
 	[[ ! -d "$rom_dir" ]] && echo "rom_dir doesn't exist" && exit 1
@@ -31,9 +31,7 @@ main() {
 
 	local rom
 	for rom in *; do
-
         rom_no_disk=$(echo "$rom" | sed 's/(Disk [^)]*)//g;s/(System)//g;s/(Data)//g;s/\[a\]//g')
-
 		echo "$rom" >> "${rom_no_disk%.*}".m3u
 	done
 
